70ab02aa5feabe197addd0cbd86d65163591f9c3
semral <semral.92@mail.ru>
2025-10-20 09:24:56 +0400

    merge

--- src/test-hot-reload.js
+++
@@ -1,6 +0,0 @@
-// Test file for hot reload
-export const testHotReload = () => {
-    console.log('Hot reload is working! 🔥');
-    // Добавьте здесь дополнительные тесты или логи
-    console.log('Тестирование горячей перезагрузки...');
-};
