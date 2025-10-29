82592532739a231dc0a42878c65b48cfa8bde227
semral <semral.92@mail.ru>
2025-10-29 16:40:03 +0400

    created

--- src/main.jsx
+++ src/main.jsx
@@ -11,4 +11,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 );
 
 // Инициализируем мониторинг Web Vitals
-initWebVitals();
+// initWebVitals();
+
+// i want here add to console log made by amiroff creative agency with link to amiroff site  with link color white
+
+
+
+console.log("%c Made with ❤️ by Amiroff Creative Agency %c https://amiroff.az/ ", "background: #222; color: #bada55; padding: 5px 0;", "background: #222; color: #ffffff; padding: 5px 0;");
+
